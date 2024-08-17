5bf3f2dd569955da4bec1c0f8252dc88a760bbbe
Hermit888 <78113396+Hermit888@users.noreply.github.com>
2024-08-16 19:33:30 -0500

    Update website.py

--- website/website.py
+++ website/website.py
@@ -14,8 +14,8 @@ st.set_page_config(layout='wide')
 
 # folderimg = os.getcwd() + '\\CO_NG_images'
 # folderimg2 = os.getcwd() + '\\dpr images'
-folderimg = os.path.abspath(os.path.join(os.getcwd(), "..")) + '\\CO_NG_images'
-folderimg2 = os.path.abspath(os.path.join(os.getcwd(), "..")) + '\\dpr images'
+folderimg = r'CO_NG_images'
+folderimg2 = r'dpr images'
 
 # get data
 @st.cache_data
